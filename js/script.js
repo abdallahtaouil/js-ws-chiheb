89b1127cd0728f0eb15c8b1eadc90ad76002dfd2
chihebgmc <chihebchabougmc@gmail.com>
2023-10-30 11:43:48 +0100

    coding session 30/10/2023

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ person1['isAlive'] = false;
 console.log(person1);
 
 // Get age of person1 with braket notation
-var key = 'age';
+var key = 'age'; //
 console.log(person1[key]);
 
 // Regular Function
@@ -74,3 +74,77 @@ person1.sayHello = function () {
 };
 console.log(person1);
 console.log(person1.sayHello());
+
+// Declaration of empty array
+var cars = [];
+
+// Add element to cars with push method
+cars.push('Opel', 'BMW', 'Peugeot');
+console.log(cars);
+
+// Sorting of array
+var numbers = [1, 30, 4, 21, 100000];
+
+numbers.sort(function (a, b) {
+  return b - a;
+});
+console.log(numbers);
+
+// Check if every number in numbers is even
+var result = numbers.every(function (number) {
+  return number % 2 === 0;
+});
+
+console.log(result);
+
+// Forof used with array
+for (let i = 0; i <= numbers.length - 1; i++) {
+  console.log(numbers[i]);
+}
+for (const number of numbers) {
+  console.log(number);
+}
+
+for (const car of cars) {
+  console.log(car);
+}
+
+// Forin used with objects
+console.log(person1);
+
+for (const key in person1) {
+  console.log(key + ': ' + person1[key]);
+}
+
+// Some built-in Object methods
+
+// Object.keys() : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+// Log to the console all person1 keys as array
+console.log(Object.keys(person1));
+
+// Object.values() : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+// Log to the console all person1 key values as array
+console.log(Object.values(person1));
+
+// Object.entries() : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+// Log to the console all person1 key value pairs as array
+console.log(Object.entries(person1));
+
+// alert()
+// alert('Bonjour');
+
+// confirm()
+// var res = confirm('Are you sure you want to continue?');
+// console.log(res);
+
+// prompt()
+var positveNumber = prompt('Enter a positve number');
+
+// Convert the prompt result from string to number
+var posetiveNumberAsNumber = +positveNumber; // or Number(positveNumber)
+
+// Log to the console content of posetiveNumberAsNumber
+console.log(posetiveNumberAsNumber);
+
+// Log to the console the type of posetiveNumberAsNumber
+console.log(typeof posetiveNumberAsNumber);
